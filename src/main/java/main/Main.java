5aa343757c606a1d5453647305e81ccd839eac00
Kari Vänttinen <kvanttinen@gmail.com>
2017-11-13 00:13:12 +0200

    codacy testi

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -5,8 +5,6 @@ import ohtu.ohtuvarasto.Varasto;
 public class Main {
 
     public static void main(String[] args) {
-
-        Varasto mehua = new Varasto(100.0);
-        Varasto olutta = new Varasto(100.0, 20.2);
+      System.out.println("JEe");
     }
 }
