58f97402507279954735138ae5edc8a91c8d23a6
Kari Vänttinen <kvanttinen@gmail.com>
2017-11-13 00:19:04 +0200

    codacy testi

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,9 @@
 package main;
 
-import ohtu.ohtuvarasto.Varasto;
 
 public class Main {
 
     public static void main(String[] args) {
-      System.out.println("JEe");
+        System.out.println("JEe");
     }
 }
